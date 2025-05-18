11bcb8e0be065f807ce3fd2f489b27eb549b07bd
Saksham <saksham.gairola28@gmail.com>
2025-05-18 21:03:17 +0530

    fix: repalce jQuery dom polling with vanilla js polling

--- routes/sports.js
+++ routes/sports.js
@@ -10,30 +10,38 @@ router.get("/", attachBrowserSession('generic'), async (req, res) => {
   logger.info("Received GET /api/games request", { action: "fetch sports" });
 
   try {
-    // await page.waitForSelector("div.accordion:nth-of-type(3) ul.navbar-nav", {
-    //   visible: true,
-    //   timeout: 10000,
-    // });
-
     const sports = await page.evaluate(() => {
-      const scrappedSports = {};
-      const allSports = $("div.accordion:nth-of-type(3) ul.navbar-nav");
-      allSports.map((_, sport) => {
-        const sportName =
-          $(sport).find("a:first > span").text().trim() ?? "Unknown Sport";
-        if (!scrappedSports[sportName]) scrappedSports[sportName] = {};
-        const allSeries = $(sport).find("li:first>ul");
-        allSeries.map((_, series) => {
-          const seriesName =
-            $(series).find("a:first > span").text().trim() ?? "Unknown Series";
-          const matchArr = [];
-          const matches = $(series).find("ul.dropdown-menu>li");
-          matches.map((_, match) => {
-            const matchName = $(match).find("a").text();
-            const matchUrl = $(match).find("a")[0].href;
-            if (matchName && matchUrl) matchArr.push({ [matchName]: matchUrl });
+      scrappedSports = {};
+      allSports = document.querySelectorAll("div.accordion:nth-of-type(3) ul.navbar-nav");
+      allSports.forEach(sportList => {
+        const sports = sportList.querySelectorAll(":scope > li");
+
+        sports.forEach(sport => {
+          const sportNameElem = sport.querySelector("a > span");
+          const sportName = sportNameElem?.textContent.trim() || "Unknown Sport";
+          if (!scrappedSports[sportName]) scrappedSports[sportName] = {};
+
+          const seriesList = sport.querySelectorAll(":scope > ul > li");
+
+          seriesList.forEach(series => {
+            const seriesNameElem = series.querySelector("a > span");
+            const seriesName = seriesNameElem?.textContent.trim() || "Unknown Series";
+            const matchArr = [];
+
+            const matches = series.querySelectorAll("ul.dropdown-menu > li");
+
+            matches.forEach(match => {
+              const anchor = match.querySelector("a");
+              const matchName = anchor?.textContent.trim();
+              const matchUrl = anchor?.href;
+
+              if (matchName && matchUrl) {
+                matchArr.push({ [matchName]: matchUrl });
+              }
+            });
+
+            scrappedSports[sportName][seriesName] = matchArr;
           });
-          scrappedSports[sportName][seriesName] = matchArr;
         });
       });
       return scrappedSports;
